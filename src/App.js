166e14c9a68551f7593df6f7802c579fdbdd4d84
Rustam <rustam905@gmail.com>
2021-05-11 12:42:48 -0700

    Containers and Grids

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ import {
 	ThemeProvider,
 	createMuiTheme,
 	Typography,
+	Container,
+	Paper,
+	Grid,
 } from "@material-ui/core";
 import { orange, green } from "@material-ui/core/colors";
 import { Save, Delete } from "@material-ui/icons";
@@ -31,7 +34,7 @@ const theme = createMuiTheme({
 	typography: {
 		h2: {
 			fontSize: 30,
-      marginBottom:15
+			marginBottom: 15,
 		},
 	},
 	palette: {
@@ -73,44 +76,50 @@ function CheckboxExample() {
 function App() {
 	return (
 		<ThemeProvider theme={theme}>
-			<div className='App'>
-				<header className='App-header'>
-					<Typography variant='h2' component='div'>
-						Welcome to Material UI
-					</Typography>
-					<Typography variant=' subtitle1'>Welcome to Material UI</Typography>
-					<ButtonStyled />
-					<TextField
-						variant='filled'
-						color='secondary'
-						type='email'
-						label='The Time'
-						placeholder='testing@gmail.com'
-					/>
-					<CheckboxExample />
-					<ButtonGroup variant='contained'>
-						<Button
-							startIcon={<Save />}
-							href='#'
-							color='primary'
-							style={{
-								fontSize: 20,
-							}}>
-							Save
-						</Button>
-						<Button
-							startIcon={<Delete />}
-							href='#'
-							color='secondary'
-							style={{
-								fontSize: 20,
-							}}>
-							Discard
-						</Button>
-					</ButtonGroup>
-					<img src={logo} className='App-logo' alt='logo' />
-				</header>
-			</div>
+			<Container maxWidth='s'>
+				<div className='App'>
+					<header className='App-header'>
+						<Typography variant='h2' component='div'>
+							Welcome to Material UI
+						</Typography>
+						<Typography variant=' subtitle1'>Welcome to Material UI</Typography>
+						<ButtonStyled />
+						<Grid container spacing={2} justify="center">
+							<Grid item>
+								<Paper style={{ height: 75, width: 50 }} />
+							</Grid>
+							<Grid item>
+								<Paper style={{ height: 75, width: 50 }} />
+							</Grid>
+							<Grid item>
+								<Paper style={{ height: 75, width: 50 }} />
+							</Grid>
+						</Grid>
+						<CheckboxExample />
+						<ButtonGroup variant='contained'>
+							<Button
+								startIcon={<Save />}
+								href='#'
+								color='primary'
+								style={{
+									fontSize: 20,
+								}}>
+								Save
+							</Button>
+							<Button
+								startIcon={<Delete />}
+								href='#'
+								color='secondary'
+								style={{
+									fontSize: 20,
+								}}>
+								Discard
+							</Button>
+						</ButtonGroup>
+						<img src={logo} className='App-logo' alt='logo' />
+					</header>
+				</div>
+			</Container>
 		</ThemeProvider>
 	);
 }
